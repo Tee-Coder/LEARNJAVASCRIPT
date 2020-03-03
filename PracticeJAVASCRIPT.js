ebb8663e93e623b0f7cf8547c34164f85d40286d
Toyin <oluwatoyinogundare@gmail.com>
2020-03-03 05:51:17 -0700

    added web files

--- PracticeJAVASCRIPT.js
+++ PracticeJAVASCRIPT.js
@@ -1,4 +1,66 @@
-var x,y,z;
+/*var x,y,z,w;
 x = 5;
-y = 6
-z =5+6;
+y = 6;
+z = x + y;
+x *=y;
+
+
+console.log(z);
+
+var i = "hello";
+var j ="Toyin";
+var k =i + j;
+
+
+console.log(i + " " + j );
+console.log(k);
+console.log(x);*/
+
+/*var Phones = ['iphone', 'Samsung', 'blackerry']
+
+Phones[0] ="Huawei";
+
+Phones [3] ='IPHONE';
+Phones.push("Iphone");
+Phones[3] ="LG";
+console.log(Phones);
+
+
+for(var l=1; l < Phones.length; l++)
+{
+    var mobile = Phones [l];
+    console.log(mobile);
+}*/
+
+/*
+var i = 40
+for (i=0; i<41; i++)
+{
+    if (i%2==0)
+    {
+        console.log(i);
+    }
+
+    else if(i%3 == 1)
+    {
+        console.log('odd numbers');
+    }
+
+    else{
+        console.log('invalid');
+    }
+}
+*/
+
+//clock conversion
+var onehour = 60;
+var fivehours = onehour*5;
+var tenhours= onehour*10;
+console.log(fivehours + ' '+'minutes'); 
+console.log(tenhours + ' ' + 'minutes');
+
+
+
+
+
+
