63a20c7247657958e354c66eaae86f5ac8e1680b
Toyin <oluwatoyinogundare@gmail.com>
2020-03-04 22:19:22 -0700

    added web files

--- formscript.js
+++ formscript.js
@@ -1,6 +1,6 @@
 
 
-function validateform(){
+function validateform(){/*
 if (document.Biodata.Name.value =="")
 {
    alert("please provide your name");
@@ -11,24 +11,54 @@ else if (document.Biodata.Age.value =="")
 {
     alert("please provide your age");
 }
+
 else if (document.Biodata.schoolclass =="")
 {
     alert("please provide your current class");
 } 
+
 else if (document.Biodata.city =="")
 {
     alert("please provide your current city");
 }
-else if (document.Biodata.Email-address =="")
+
+else if (document.Biodata.Email =="")
 {
     alert("please enter your email address");
 }
 
-else{
-    
+else
+*/
+   var Name = document.getElementById("Name").value;
+   var Age = document.getElementById("Age").value;
+   var Schoolclass = document.getElementById("Schoolclass").value;
+   var city = document.getElementById("city").value;
+   var Email = document.getElementById("Email").value;
+
+   var InputDetails = [Name, Age, Schoolclass, city, Email];
+
+   var tablebody = document.getElementById('form-body');
+   var newRow = document.createElement('tr');
+   
+   for(var c=0; c < InputDetails; c++){
+   var td = document.createElement('td');
+   td.innerHTML = InputDetails(c);
+   newRow.appendChild(td);
+   }
+
+tablebody.appendChild(newRow);
+
+
+
+Name= document.getElementById ("Name").value ="";
+Age= document.getElementById ("Age").value ="";
+Schoolclass= document.getElementById ("Schoolclass").value ="";
+Email= document.getElementById ("Email").value ="";
+city= document.getElementById ("city").value ="";
 }
 
-}
+
+
 
 
 /*
